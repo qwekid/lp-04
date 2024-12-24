9a2542b16fb967b696862f1f458528a6435e2905
qwekid <kraevdmitrii.zamt@gmail.com>
2024-12-24 14:37:07 +0300

    added support of pwd with keys -P&-L

--- pwd.go
+++ pwd.go
@@ -1,18 +1,56 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	// Получаем текущий рабочий каталог
-	dir, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Ошибка:", err)
-		return
+	// Определяем флаги
+	logical := flag.Bool("L", false, "выводит логический путь")
+	physical := flag.Bool("P", false, "выводит физический путь")
+
+	// Парсим флаги
+	flag.Parse()
+
+	// Получаем текущую директорию
+	var currentDir string
+	var err error
+
+	if *physical {
+		// Получаем физический путь
+		currentDir, err = os.Getwd()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Ошибка получения физического пути: %v", err)
+			os.Exit(1)
+		}
+	} else {
+		if *logical {
+			// Получаем логический путь
+			currentDir, err = os.Readlink("/proc/self/cwd")
+			if err != nil {
+				// Если нет доступа к /proc/self/cwd, используем os.Getwd()
+				currentDir, err = os.Getwd()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Ошибка получения логического пути: %v", err)
+					os.Exit(1)
+				}
+			}
+		} else{
+			// Получаем логический путь
+			currentDir, err = os.Readlink("/proc/self/cwd")
+			if err != nil {
+				// Если нет доступа к /proc/self/cwd, используем os.Getwd()
+				currentDir, err = os.Getwd()
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "Ошибка получения логического пути: %v", err)
+					os.Exit(1)
+				}
+			}
+		}
 	}
 
-	// Выводим текущий рабочий каталог
-	fmt.Println(dir)
+
+	fmt.Println(currentDir)
 }
