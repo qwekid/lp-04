2c4f0640fdb5bbff6b86fa62ddbc4ed4af608516
qwekid <kraevdmitriy.zamt@gmail.com>
2024-12-25 23:53:45 +0300

    close #13

--- exit.go
+++ exit.go
@@ -1,50 +1,59 @@
 package main
 
 import (
-"fmt"
-"os"
-"strings"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
-args := os.Args[1:]
+	// Обработка ключа -help
+	help := flag.Bool("help", false, "Показать помощь и выйти")
+	flag.Parse()
 
-if len(args) == 0 || args[0] == "help" {
-	printHelp()
-	return
-}
+	if *help {
+		printHelp()
+		os.Exit(0)
+	}
 
-exitCode := 0
-exitMessage := ""
+	// Получение аргументов командной строки (без имени программы)
+	args := flag.Args()
 
-switch args[0] {
-case "-f":
-	exitCode = 1
-	exitMessage = "Фатальная ошибка"
-case "-s":
-	exitCode = 0
-	exitMessage = "Успешное завершение программы"
-case "-r":
-	exitCode = 2
-	exitMessage = "Ошибка ввода данных"
-default:
-	fmt.Println("Неизвестный ключ. Используйте ключ help для справки.")
-	return
-}
+	if len(args) != 1 {
+		fmt.Println("Ошибка: требуется один аргумент для завершения с указанным кодом.")
+		fmt.Println("Используйте -help для получения информации.")
+		os.Exit(1)
+	}
 
-fmt.Println(exitMessage)
-os.Exit(exitCode)
+	// Попытка преобразования аргумента в код завершения
+	exitCode, err := parseExitCode(args[0])
+	if err != nil {
+		fmt.Printf("Ошибка: %s\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("Завершение программы с кодом %d\n", exitCode)
+	os.Exit(exitCode)
 }
 
 func printHelp() {
-helpText := `
-Программа для завершения работы с заданным кодом.
+	fmt.Println("Использование: exit [код завершения]")
+	fmt.Println("Ключи:")
+	fmt.Println("  -help       Показать это сообщение и выйти")
+	fmt.Println("Аргументы:")
+	fmt.Println("  [код завершения] Укажите целое число в диапазоне 0-255 для завершения программы с этим кодом.")
+}
 
-Использование:
-- ./exit -f   : завершение с фатальной ошибкой
-- ./exit -s   : успешное завершение программы
-- ./exit -r   : завершение с ошибкой ввода данных
-- ./exit help : показать справку
-`
-fmt.Println(strings.TrimSpace(helpText))
+func parseExitCode(arg string) (int, error) {
+	var code int
+	_, err := fmt.Sscanf(arg, "%d", &code)
+	if err != nil {
+		return 0, fmt.Errorf("некорректный код завершения: %s", arg)
+	}
+
+	if code < 0 || code > 255 {
+		return 0, fmt.Errorf("код завершения должен быть в диапазоне 0-255, получено: %d", code)
+	}
+
+	return code, nil
 }
