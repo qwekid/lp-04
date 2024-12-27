b72a90b8285c401dafa24c1654bcf12f9b697f05
user <user@localhost.localdomain>
2024-12-27 15:15:04 +0300

    bug fix

--- df.go
+++ df.go
@@ -1,116 +1,106 @@
 package main
 
 import (
-"flag"
-"fmt"
-"syscall"
+	"fmt"
+	"os"
+	"syscall"
 )
-
-// Структура для хранения информации о файловой системе
-type FileSystemInfo struct {
-Filesystem string
-Type string
-Size uint64
-Used uint64
-Free uint64
-UsagePerc float64
-InodesTotal uint64
-InodesUsed uint64
-InodesFree uint64
-}
-
-// Константы для единиц измерения
-const (
-KB = 1024
-MB = KB * 1024
-GB = MB * 1024
-)
-
-// Функция для конвертации размера в читаемый формат
-func humanReadableSize(size uint64) string {
-switch {
-case size >= GB:
-return fmt.Sprintf("%.1fG", float64(size)/float64(GB))
-case size >= MB:
-return fmt.Sprintf("%.1fM", float64(size)/float64(MB))
-case size >= KB:
-return fmt.Sprintf("%.1fK", float64(size)/float64(KB))
-default:
-return fmt.Sprintf("%dB", size)
-}
-}
-
-// Получаем информацию о файловых системах с помощью syscall.Statfs
-func getFileSystemInfo(path string) (FileSystemInfo, error) {
-var stat syscall.Statfs_t
-err := syscall.Statfs(path, &stat)
-if err != nil {
-return FileSystemInfo{}, fmt.Errorf("не удалось получить информацию о файловой системе для %s: %v", path, err)
-}
-
-fs := FileSystemInfo{}
-fs.Filesystem = path
-fs.Size = uint64(stat.Blocks) * uint64(stat.Bsize)
-fs.Free = uint64(stat.Bfree) * uint64(stat.Bsize)
-fs.Used = fs.Size - fs.Free
-fs.UsagePerc = float64(fs.Used) / float64(fs.Size) * 100
-fs.InodesTotal = uint64(stat.Files)
-fs.InodesFree = uint64(stat.Ffree)
-fs.InodesUsed = fs.InodesTotal - fs.InodesFree
-
-return fs, nil
-}
-
-// Выводим информацию о файловых системах
-func printFileSystems(fs FileSystemInfo, humanReadable bool, showInodes bool) {
-if humanReadable {
-fmt.Printf("%-20s %-20s %-20s %-20s %-20s\n", fs.Filesystem, humanReadableSize(fs.Size), humanReadableSize(fs.Free), humanReadableSize(fs.Used), fmt.Sprintf("%.2f%%", fs.UsagePerc))
-} else {
-fmt.Printf("%-20s %-20d %-20d %-20d %-20.2f%%\n", fs.Filesystem, fs.Size, fs.Free, fs.Used, fs.UsagePerc)
-}
-
-if showInodes {
-	fmt.Printf("  Inodes: %d (занято %d, свободно %d)\n", fs.InodesTotal, fs.InodesUsed, fs.InodesFree)
-}
-fmt.Println()
-}
 
 func printHelp() {
-fmt.Println("Использование: df [опции] [путь]")
-fmt.Println("Опции:")
-fmt.Println(" -h Выводить размеры в удобочитаемом формате (например, 1K, 234M)")
-fmt.Println(" -i Показывать информацию о индексных узлах (inodes)")
-fmt.Println(" -help Показать справку")
+	fmt.Println("Использование: df [ОПЦИИ]")
+	fmt.Println("Вывод информации о файловых системах.")
+	fmt.Println()
+	fmt.Println("ОПЦИИ:")
+	fmt.Println("  -h       Показать размеры в удобочитаемом формате.")
+	fmt.Println("  -T       Показать тип файловой системы.")
+	fmt.Println("  --help   Показать эту справку и выйти.")
+}
+
+func getFilesystemType(fsPath string) (string, error) {
+	var stat syscall.Stat_t
+	err := syscall.Stat(fsPath, &stat)
+	if err != nil {
+		return "", err
+	}
+
+	// Получаем тип файловой системы
+	var statfs syscall.Statfs_t
+	err = syscall.Statfs(fsPath, &statfs)
+	if err != nil {
+		return "", err
+	}
+
+	// Используем f_type для определения типа файловой системы
+	switch statfs.Type {
+	case 0xEF53: // ext2/ext3/ext4
+		return "ext4", nil
+	case 0xFF534D42: // NTFS
+		return "ntfs", nil
+	case 0x9FA0: // tmpfs
+		return "tmpfs", nil
+	default:
+		return "неизвестно", nil
+	}
 }
 
 func main() {
-// Флаги командной строки
-showHelp := flag.Bool("help", false, "Показать справку")
-humanReadable := flag.Bool("h", false, "Выводить размеры в удобочитаемом формате (например, 1K, 234M)")
-showInodes := flag.Bool("i", false, "Показывать информацию о индексных узлах (inodes)")
+	if len(os.Args) > 1 && os.Args[1] == "--help" {
+		printHelp()
+		return
+	}
 
-// Парсим флаги
-flag.Parse()
+	humanReadable := false
+	showType := false
 
-// Показать справку
-if *showHelp {
-	printHelp()
-	return
-}
+	for _, arg := range os.Args[1:] {
+		switch arg {
+		case "-h":
+			humanReadable = true
+		case "-T":
+			showType = true
+		default:
+			printHelp()
+			return
+		}
+	}
 
-// Если путь не передан, используем текущую директорию
-path := "."
-if len(flag.Args()) > 0 {
-	path = flag.Args()[0]
-}
+	// Получаем информацию о файловых системах
+	fs := syscall.Statfs_t{}
+	filesystems := []string{"/", "/home", "/var"} // Пример файловых систем
 
-// Получаем информацию о файловой системе
-fs, err := getFileSystemInfo(path)
-if err != nil {
-	fmt.Println("Ошибка:", err)
-	return
-}
+	fmt.Printf("%-20s %-10s %-10s %-10s \n", "Файловая система", "Размер", "Использовано", "Доступно")
+	for _, fsPath := range filesystems {
+		err := syscall.Statfs(fsPath, &fs)
+		if err != nil {
+			fmt.Printf("Ошибка получения информации о %s: %v \n", fsPath, err)
+			continue
+		}
 
-// Выводим информацию о файловой системе
-printFileSystems(fs, *humanReadable, *showInodes)
+		total := fs.Blocks * uint64(fs.Bsize)
+		free := fs.Bfree * uint64(fs.Bsize)
+		used := total - free
+
+		var totalStr, usedStr, freeStr string
+		if humanReadable {
+			totalStr = fmt.Sprintf("%.1fG", float64(total)/1024/1024)
+			usedStr = fmt.Sprintf("%.1fG", float64(used)/1024/1024)
+			freeStr = fmt.Sprintf("%.1fG", float64(free)/1024/1024)
+		} else {
+			totalStr = fmt.Sprintf("%d", total)
+			usedStr = fmt.Sprintf("%d", used)
+			freeStr = fmt.Sprintf("%d", free)
+		}
+
+		var fsType string
+		if showType {
+			fsType, err = getFilesystemType(fsPath)
+			if err != nil {
+				fmt.Printf("Ошибка получения типа файловой системы для %s: %v \n", fsPath, err)
+				fsType = "неизвестно"
+			}
+			fmt.Printf("%-20s %-10s %-10s %-10s %-10s \n", fsPath, totalStr, usedStr, freeStr, fsType)
+		} else {
+			fmt.Printf("%-20s %-10s %-10s %-10s \n", fsPath, totalStr, usedStr, freeStr)
+		}
+	}
 }
